83b9ccdab1c665138964ae808e0f6e009b7f52b5
Jonathan Dextraze <Jonathan Dextraze>
2016-03-28 15:23:11 -0700

    firt version

---
+++ client.go
@@ -0,0 +1,23 @@
+package digitalocean
+
+import (
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+)
+
+type TokenSource struct {
+	AccessToken string
+}
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+func NewClient(token string) *godo.Client {
+	tokenSource := &TokenSource{AccessToken: token}
+	oauthClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
+	return godo.NewClient(oauthClient)
+}
